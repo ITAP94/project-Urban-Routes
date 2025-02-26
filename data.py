155836d28397a8f0f78614737964c45466784781
INGRID ALVARADO <italoderecho@gmail.com>
2025-02-26 18:01:04 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+urban_routes_url = 'https://cnt-fc07f046-3555-4296-b2d9-49b9ef1d1746.containerhub.tripleten-services.com/?lng=es'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Muéstrame el camino al museo'
